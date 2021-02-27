39a388b2684527f0427962e6413ac630b8b65b9d
kala476 <kala476@gmail.com>
2021-02-27 14:49:28 +0100

    playe script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public Collider BallHitZone;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void HitBall(float force, Vector2 hitDirection)
+	{
+
+	}
+
+
+}
